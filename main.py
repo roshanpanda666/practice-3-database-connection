f57a1c7ed1ba6096c42c333145d289d9cc520fb1
roshan <roshanpanda666@gmail.com>
2025-07-17 22:05:35 +0530

    Update UI and add user entry functionality with data insertion

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import customtkinter as ctk
 from db import get_all_data
 import threading
+from db import insert_data
+
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
 
 app = ctk.CTk()
-app.geometry("600x600")
+app.geometry("700x600")
 app.title("MongoDB Viewer")
 
 label = ctk.CTkLabel(app, text="📡 Waiting for data...")
@@ -27,7 +29,7 @@ def start_function():
 
 btn = ctk.CTkButton(
     master=app,
-    text="Launch 🚀",
+    text="show logs",
     fg_color="#2a2a40",
     hover_color="#444",
     text_color="white",
@@ -36,5 +38,52 @@ btn = ctk.CTkButton(
     command=start_function
 )
 btn.pack(pady=12)
+
+
+def getuserentry():
+    print("User entry function called")
+    username=entry.get()
+    password=entry2.get()
+    email=entry3.get()
+    label2.configure(text=f"Welcome, {username} 🔥")
+    label3.configure(text=f"email:{email}")
+    label4.configure(text=f"password:{password}")
+    print(username)
+
+    user={"name": username, "password": password, "email": email}
+    insert_id=insert_data(user)
+    print("data inserted ",insert_id)
+
+
+entry = ctk.CTkEntry(app, placeholder_text="enter username", width=300, height=40)
+entry.pack(pady=5)
+
+entry2 = ctk.CTkEntry(app, placeholder_text="enter password", width=300, height=40)
+entry2.pack(pady=5)
+
+entry3 = ctk.CTkEntry(app, placeholder_text="enter email", width=300, height=40)
+entry3.pack(pady=5)
+
+
+btn = ctk.CTkButton(
+    master=app,
+    text="Submit 🚀",
+    command=getuserentry,
+    font=("Verdana", 14, "bold"),
+    fg_color="#1f1f1f",
+    hover_color="#333",
+    text_color="white",
+)
+btn.pack(pady=10)
+
+# 📺 Label to display result
+label2 = ctk.CTkLabel(app, text="")
+label2.pack(pady=10)
+label3 = ctk.CTkLabel(app,text="")
+label3.pack(pady=10)
+label4 = ctk.CTkLabel(app,text="")
+label4.pack(pady=10)
+
+print(entry)
 
 app.mainloop()
